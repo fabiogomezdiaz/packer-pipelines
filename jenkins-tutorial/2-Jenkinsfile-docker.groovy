5ac609008243d9a7c65746d8f86139f25116f069
Fabio A. Gomez Diaz <fabiogomezdiaz@outlook.com>
2021-08-16 17:26:49 -0500

    - Using Docker

---
+++ jenkins-tutorial/2-Jenkinsfile-docker.groovy
@@ -0,0 +1,29 @@
+pipeline {
+  agent none
+
+  stages {
+    stage('Packer - Build Docker Image') {
+      agent {
+        docker {
+          image 'hashicorp/packer:light'
+        }
+      }
+      steps {
+        sh """
+        #!/bin/bash
+        cd jenkins-tutorial
+        packer init .
+        packer build -force .
+        """
+      }
+    }
+    /*stage('Docker - Verify Docker Image') {
+      steps {
+        sh """
+        #!/bin/bash
+        docker images
+        """
+      }
+    }*/
+  }
+}
