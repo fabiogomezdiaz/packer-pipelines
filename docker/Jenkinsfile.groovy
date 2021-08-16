4031c0fa55f00b2a08cad89e17f67c4baa7f7b77
Fabio A. Gomez Diaz <fabiogomezdiaz@outlook.com>
2021-08-16 12:17:46 -0500

    - Packer Docker Pipeline

---
+++ docker/Jenkinsfile.groovy
@@ -0,0 +1,20 @@
+pipeline {
+  agent any
+
+  stages {
+    stage('Packer - Build Docker Image') {
+      steps {
+        echo 'Building Ubuntu Container Image using Packer...'
+        sh 'cd docker'
+        sh 'packer init .'
+        sh 'packer build -force .'
+      }
+    }
+    stage('Docker - Verify Docker Image') {
+      steps {
+        echo 'Verifying Container Image using Docker..'
+        sh 'docker images'
+      }
+    }
+  }
+}
