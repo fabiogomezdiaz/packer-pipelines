3e8ab6b999f02f9813dacc488c65457781c70e05
Fabio A. Gomez Diaz <fabiogomezdiaz@outlook.com>
2021-08-16 12:42:42 -0500

    - Setting path

--- docker/Jenkinsfile.groovy
+++ docker/Jenkinsfile.groovy
@@ -5,12 +5,12 @@ pipeline {
     stage('Setting PATH') {
       steps {
         echo 'Setting PATH for Packer...'
-        sh 'export PATH="/usr/local/bin:${PATH}" && echo $PATH'
-        sh 'echo $PATH'
+        sh 'export PATH="/usr/local/bin:${PATH}"'
       }
     }
     stage('Packer - Build Docker Image') {
       steps {
+        sh 'export PATH="/usr/local/bin:${PATH}"'
         echo 'Building Ubuntu Container Image using Packer...'
         sh 'cd docker'
         sh 'packer init .'
@@ -19,6 +19,7 @@ pipeline {
     }
     stage('Docker - Verify Docker Image') {
       steps {
+        sh 'export PATH="/usr/local/bin:${PATH}"'
         echo 'Verifying Container Image using Docker..'
         sh 'docker images'
       }
