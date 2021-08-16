f01ad62339fa4d886e62d4da5d7a3e61811c6499
Fabio A. Gomez Diaz <fabiogomezdiaz@outlook.com>
2021-08-16 17:35:35 -0500

    - Testing

--- jenkins-tutorial/2-Jenkinsfile-docker.groovy
+++ jenkins-tutorial/2-Jenkinsfile-docker.groovy
@@ -1,12 +1,29 @@
 pipeline {
-    agent {
-        docker { image 'node:14-alpine' }
-    }
-    stages {
-        stage('Test') {
-            steps {
-                sh 'node --version'
-            }
+  agent none
+
+  stages {
+    stage('Packer - Build Docker Image') {
+      agent {
+        docker {
+          image 'hashicorp/packer:light'
         }
+      }
+      steps {
+        sh """
+        #!/bin/bash
+        cd jenkins-tutorial
+        packer init .
+        packer build -force .
+        """
+      }
     }
+    /*stage('Docker - Verify Docker Image') {
+      steps {
+        sh """
+        #!/bin/bash
+        docker images
+        """
+      }
+    }*/
+  }
 }
