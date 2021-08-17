cba2f39a6285e15f83e965ba0007dcf339202cb0
Fabio A. Gomez Diaz <fabiogomezdiaz@outlook.com>
2021-08-17 09:06:35 -0500

    - Added docker in docker container

--- jenkins-tutorial/2-Jenkinsfile-docker.groovy
+++ jenkins-tutorial/2-Jenkinsfile-docker.groovy
@@ -12,16 +12,19 @@
 }*/
 
 pipeline {
-  agent none
+  agent {
+    docker {
+      image 'hashicorp/packer:light'
+      args "-e PACKER_PLUGIN_PATH='.packer.d/plugins' --entrypoint=''"
+    }
+    docker {
+      image 'docker:20.10-dind'
+      args "--privileged"
+    }
+  }
 
   stages {
     stage('Packer - Build Docker Image') {
-      agent {
-        docker {
-          image 'hashicorp/packer:light'
-          args "-e PACKER_PLUGIN_PATH='.packer.d/plugins' --entrypoint=''"
-        }
-      }
       steps {
         sh """
         echo ${WORKSPACE}
