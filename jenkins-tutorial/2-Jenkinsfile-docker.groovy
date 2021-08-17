771f435c51a6b8096cc110aec48fc9f7d5bf75f5
Fabio A. Gomez Diaz <fabiogomezdiaz@outlook.com>
2021-08-17 08:50:08 -0500

    - Changing path

--- jenkins-tutorial/2-Jenkinsfile-docker.groovy
+++ jenkins-tutorial/2-Jenkinsfile-docker.groovy
@@ -19,7 +19,7 @@ pipeline {
       agent {
         docker {
           image 'hashicorp/packer:light'
-          args "-v ${env.WORKSPACE}:/workspace -w /workspace -e PACKER_PLUGIN_PATH=/workspace/.packer.d/plugins --entrypoint=''"
+          args "-v ${env.WORKSPACE}/jenkins-tutorial:/workspace -w /workspace -e PACKER_PLUGIN_PATH=/workspace/.packer.d/plugins --entrypoint=''"
         }
       }
       steps {
